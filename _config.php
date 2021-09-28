d64d88467916807ae0a1c2e6d6d8dfad6a1b7c5e
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-09-28 22:42:20 +1300

    WIP

---
+++ _config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * developed by www.sunnysideup.co.nz
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+ *
+ */
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START campaignmonitor MODULE ----------------===================
+
+//===================---------------- END campaignmonitor MODULE ----------------===================
+
+define('SS_CAMPAIGNMONITOR_DIR', 'campaignmonitor');
